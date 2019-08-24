6e0c4d30331785b0de31a3b2e5547d9221ee2d03
arawo <araw0@outlook.com>
2019-08-24 14:04:37 +0900

    🦍ゴリラを追加、ウホウホ

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module jig-hackathon-c
 
 go 1.12
+
+require github.com/gorilla/mux v1.7.3
--- server.go
+++ server.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	fmt.Println("Hello, World!!")
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "Hello, world!!")
+	}).Methods("GET")
+
+	//r.PathPrefix("/static/").Handler(http.StripPrefix("/public/", http.FileServer(http.Dir(dir))))
+
+	srv := &http.Server{
+		Handler: r,
+		Addr:    "127.0.0.1:8000",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
 }
