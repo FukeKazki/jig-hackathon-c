c148ba3ba078d05f241dfb7fef9e0d3d1660e3e3
kimflip <k.kimusan@gmail.com>
2019-08-24 15:11:33 +0900

    csvToList

--- src/algorithm.py
+++ src/algorithm.py
@@ -2,6 +2,12 @@ import csv
 import pandas as pd
 import pprint
 
-data = pd.read_csv("./data/dummy.csv").values.tolist()
+data = pd.read_csv("./data/dummy2.csv").values.tolist()
 
-print (data)
+print (data[3])
+
+for row in data:
+    print("-------")
+    print(row[2])
+    print(row[3])
+    if 
